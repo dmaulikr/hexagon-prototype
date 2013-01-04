36d4ed5ce0952e69467bcbe8e870a01bbb09006b
Kyle Stevens <kvstevns@gmail.com>
2013-01-04 11:59:33 -0500

    Initial Commit

---
+++ HexagonPrototype/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HexagonPrototype
+//
+//  Created by Kyle Stevens on 1/4/13.
+//  Copyright kilovolt 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
